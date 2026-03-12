20f746e7efbf368d54d5ff927497c23ee24f32ad
Lucas Daniel Rocha de Oliveira <2024105070008@aluno.ifro.edu.br>
2026-03-12 11:16:02 -0400

    Primeiro commit: Ola Mundo em C++

---
+++ g++vars.bat
@@ -0,0 +1,3 @@
+@echo off
+SET "PATH=%PATH%;C:\Users\2024105070008\Desktop\compilador c++\mingw64\bin"
+echo Ambiente C++ configurado! Agora voce pode usar 'g++'.
---
+++ seila.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+
+int main()  {
+    system("cls");
+
+    std::cout << "Ola Mundo" << std::endl;
+    // std::string nome;
+    // std::cin >> nome;
+    std::string entrada;
+
+    while (entrada != "exit")
+        std::cin >> entrada;
+    
+    
+    
+    
+    
+    
+    
+return 0;
+}
