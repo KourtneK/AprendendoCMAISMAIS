33b495ad8ee5b9474cd6d3b47782faae581572e5
KourtneK <lucasdaniel2009@gmail.com>
2026-03-24 20:30:46 -0400

    nomes dos arquivos mudados, agora mostra o conteudo do arquivo no termianl

---
+++ arq.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "sair.h"
+
+
+/*
+std::ofstream: (Output File) Serve para escrever.
+std::ifstream: (Input File) Serve para ler.
+std::fstream: (File Stream) Ler e escrever, mas exige que você diga o que quer fazer na hora de abrir.
+
+
+*/
+
+// MEXENDO COM ARQUIVOS COM C++
+
+int main() {
+    std::ofstream arquivo("first file.txt");
+    std::ifstream ler_arquivo;
+
+    if (arquivo.is_open())
+    {
+        arquivo << "arquivo criado";
+        arquivo.close();
+
+        ler_arquivo.open("first file.txt");
+        
+    }
+
+    std::string conteudo_arquivo;
+    conteudo_arquivo = "first file.txt";
+
+    while (std::getline(ler_arquivo, conteudo_arquivo))
+    {
+        std::cout << "conteudo do arquivo: " << conteudo_arquivo << std::endl;
+    }
+
+    ler_arquivo.close();
+
+
+
+
+
+
+
+
+
+    return 0;
+}
