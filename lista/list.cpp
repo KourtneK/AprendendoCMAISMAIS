560930daa63b85ec4647d3c55aaf14d53e54f1ed
KourtneK <lucasdaniel2009@gmail.com>
2026-04-07 21:48:05 -0400

    pong

--- lista/list.cpp
+++ lista/list.cpp
@@ -1,85 +1,201 @@
-#include "lista.h"
 #include <iostream>
+#include <conio.h>
+#include <time.h>
+#include <Windows.h>
 
-void displayList(bool success, Alist<short> &shortList);
+enum eDirection{STOP, RIGHT, UPRIGHT, DOWNRIGHT, LEFT, UPLEFT, DOWNLEFT};
+enum eControler{PAUSE, UP, DOWN};
 
-void checkState(Alist<short> &shortList);
-
-int main()
+class cBall
 {
-    Alist<short> shortList(3);
+    private:
+            int x, y;
+            int originalX, originalY;
 
-    short num;
+            std::eDirection dir;
 
-    int i = 0;
+    public:
+            cBall(int posX, int posY)
+            {
+                originalX = posX;
+                originalY = posY;
 
-    bool success;
+                x = posX;
+                y = posY;
 
-    checkState(shortList);
+                dir = STOP;
+            }
 
-    num = 10;
+            void changeDirection(eDirection direction)
+            {
+                dir = direction;
+            }
 
-    success = shortList.insertFront(num);
+            void randomDirection()
+            {
+                srand(time(0));
+                
+                dir = (eDirection)((rand() % 6) + 1);
+            }
 
-    if (success)
-        std::cout << "insert front: " << num << std::endl;
+            inline int getX() {return x;}
+            inline int getY() {return y;}
+            inline int getDir() {return dir;}
 
-    displayList(success, shortList);
+            void move()
+            {
+                switch (dir) 
+                {
+                    case STOP:
+                        break;
+                    case RIGHT:
+                        x++;
+                        break;
+                    case UPRIGHT:
+                        x++;
+                        y--;
+                        break;
+                    case DOWNRIGHT:
+                        x++;
+                        y++;
+                        break;
+                    case LEFT:
+                        x--;
+                        break;
+                    case UPLEFT:
+                        x--;
+                        y--;
+                        break;
+                    case DOWNLEFT:
+                        x--;
+                        y++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+};
 
-    num = 20;
-
-    success = shortList.insertAtIndex(num, i);
-
-    if (success)
-        std::cout << "insert index: " << i
-                    << ": " << num << std::endl;
-
-    displayList(success, shortList);
-
-    num = 30;
-
-    success = shortList.insertBack(num, i);
-
-    if (success)
-        std::cout << "insert back: " << num << std::endl;
-
-    displayAlist(success, shortList);
-
-    return 0;
-}
-
-void displayAlist(bool success, Alist<short> &shortList)
+class cPaddle
 {
-    short min;
+    private:
+            int x, y;
+            int originalX, originalY;
 
-    int cap,
-        numVal;
+            std::eControler joystick;
     
-    if (success)
-    {
-        short.display();
-        short.getSamallest(min);
-        cap = short.getCapacity();
-        numVal = short.getNumValues();
+    public:
+            cPddle(int posX, int posY)
+            {
+                originalX = posX;
+                originalY = posY;
 
-        if (numVal > 0)
-            std::cout << "capacity: " << cap
-                        << "\tnumVal: " << numVal
-                        << "\tsmallest: " << min
-                        << std::endl;
-        else
-            std::cout << "capacity: " << cap
-                        << "\tnumvalues: " << numVal
-                        << std::endl;
-    }
+                x = posX;
+                y = posY;
 
-    checkState(shortList);
-}
+                joystick = PAUSE;
+            }
+            
+            void reset()
+            {
+                x = originalX;
+                y = originalY;
 
-void CheckState(Alist <short> &shortList)
+                joystick = PAUSE;
+            }
+
+            inline int getX() {return x;}
+            inline int getY() {return y;}
+
+            void changeDirection(eControler dir) {joystick = dir;}
+
+            void move()
+            {
+                switch(joystick)
+                {
+                    case UP:
+                        y--;
+                        break;
+                    case DOWN:
+                        y++;
+                        break;
+                    case PAUSE:
+                        break;
+                    default:
+                        break;
+                }
+            }
+};
+
+class cGameMnaganer
 {
-    if (shortList.isFull())
-        std::cerr << "list is full\n\n";
-    else if (shortList.isEmpty())
-        std::cerr << "List is empty\n\n";
+    private:
+            int width, height;
+            int score1, score2;
+
+            bool quit;
+
+            cBall* ball;
+            cPaddle* player1;
+            cPaddle *player2;
+
+    public:
+            cGameManager()
+            {
+                quit = false;
+                height = 15;
+                width = 50;
+                score1 = 0;
+                score2 = 0;
+                ball = new cBall(width / 2, height / 2);
+                player1 = new cPaddle(1, height / 2);
+                player2 = new cPaddle(width - 2, height / 2);
+            }
+
+            ~cGameManager()
+            {
+                delete ball;
+                delete player1;
+                delete player2;
+            }
+
+            void ScoreUp(cPaddle* player)
+            {
+                if (player == player1)
+                    score1++;
+                else if (player == player2)
+                    score2++;
+
+                ball->reset();
+                player1->reset();
+                player2->reset();
+            }
+
+            void Draw()
+            {
+                SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0, 0 });
+                
+                for (int i = 0; i < width + 2; i++)
+                    std::cout << "\xB2";
+
+                std::cout << std::endl;
+
+                for (int j = 0; j < width ; j++)
+                {
+                    for (int j = 0; j < width; j++)
+                    {
+                        int ballX = ball->getX();
+                        int ballY = ball->getY();
+                        int player1x = player1->getX();
+                        int player1y = player1->getX();
+                        int player2x = player2->getX();
+                        int player2y = player2->getX();
+
+                        if (j == 0)
+                            std::cout << "\xB1";
+
+                        if ()
+                    }
+                }
+            }
 }
