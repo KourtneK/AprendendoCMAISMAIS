d8c134b61d57347146f738376718c02f0330d391
KourtneK <lucasdaniel2009@gmail.com>
2026-04-14 21:51:53 -0400

    variaves unicas foram trocadas por variaveis globais, quadrado foi transformado em um objeto, triangulo adicionado

--- render/poly.cpp
+++ render/poly.cpp
@@ -27,22 +27,51 @@ int main(int argc, char *argv[])
     bool winOpen = true;
     SDL_Event evento;
 
-    SDL_FRect square; // Quadrado
-        square.x = 220.0f;
-        square.y = 340.0f;
-        square.w = 50.0f;
-        square.h = 50.0f;
+    float gravidade;
+    gravidade = 0.3f;
 
-    SDL_FRect triangle; // Triangulo
-        triangle.x = 220.0f;
-        triangle.h = 340.0f;
-        triangle.y = 340.0f;
-        triangle.w = 50.0f;
+    // Variaveis globais de estrutura de objetos
+    struct Objetos
+    {
+        SDL_FRect rect;
+            float peso;
+            float velocidade;
 
-        float triangleBase;
-        triangleBase = triangle.y + triangle.h;
+            bool state;
+    };
 
+    // Definição dos objetos
+    Objetos square; // Definição de formato, peso e estado do quadrado
+        square.rect = {220.0f, 340.0f, 50.0f, 50.0f};
+        square.peso = 1.0f;
+        square.state = false;
 
+    Objetos triangle; // Definição de formato, peso e estado do triangulo
+        triangle.rect = {220.0f, -240.0f, 50.0f, 50.0f};
+        triangle.peso = 3.0f;
+        triangle.state = false;
+
+    SDL_Vertex vertice[3]; // Vertices do triangulo
+
+        // Primeiro vertice
+        vertice[0].position.x = triangle.rect.x + (triangle.rect.w / 2.0f);
+        vertice[0].position.y = triangle.rect.y;
+        vertice[0].color = {1.0f, 0.5f, 0.0f, 1.0f}; // Cor do vertice
+        vertice[0].tex_coord = {0.0f, 0.0f}; // Não sei
+
+        // Primeiro vertice
+        vertice[1].position.x = triangle.rect.x;
+        vertice[1].position.y = triangle.rect.y + triangle.rect.h;
+        vertice[1].color = {1.0f, 0.5f, 0.0f, 1.0f};
+        vertice[1].tex_coord = {0.0f, 0.0f};
+
+        // Primeiro vertice
+        vertice[2].position.x = triangle.rect.x + triangle.rect.w;
+        vertice[2].position.y = triangle.rect.y + triangle.rect.h;
+        vertice[2].color = {1.0f, 0.5f, 0.0f, 1.0f};
+        vertice[2].tex_coord = {0.0f, 0.0f};
+
+    // Retangulos
     SDL_FRect chao; // Chão
         chao.x = 0.0f;
         chao.y = 450.0f;
@@ -72,18 +101,6 @@ int main(int argc, char *argv[])
     paredes.push_back(paredeDireita);
     paredes.push_back(paredeEsquerda);
 
-    float gravidade;
-    gravidade = 0.3f;
-
-    float squarePeso;
-    squarePeso = 1.0f;
-
-    float velocidade;
-    velocidade = 0.0f;
-
-    bool state;
-    state = false;
-
     while (winOpen)
     {
         while (SDL_PollEvent(&evento))
@@ -107,8 +124,8 @@ int main(int argc, char *argv[])
 
         float oldX;
         float oldY;
-        oldX = square.x;
-        oldY = square.y;
+        oldX = square.rect.x;
+        oldY = square.rect.y;
 
         // 3. Chama o renderizador escolhe a cor para a tela (RGB(vermelho, verde, azul), Opacidade)
         // Verde escuro(0, 140, 0, 255)
@@ -135,43 +152,58 @@ int main(int argc, char *argv[])
 
         // Seta a cor do quadrado e renderiza ele na tela
         SDL_SetRenderDrawColor(render, 0, 0, 140, 255);
-        SDL_RenderFillRect(render, &square);
+        SDL_RenderFillRect(render, &square.rect);
+
+        // Renderiza o triangulo na tela usano 'RenderGeometry'
+        SDL_RenderGeometry(render, nullptr, vertice, 3, nullptr, 0);
 
         // 5. Mostra tudo na tela
         SDL_RenderPresent(render);
 
         // Movimento. Muda as coordenadas diretamente com as setas
-        if (keyState[SDL_SCANCODE_DOWN]) square.y += 5.0f;
-        if (keyState[SDL_SCANCODE_LEFT]) square.x -= 5.0f;
-        if (keyState[SDL_SCANCODE_RIGHT]) square.x += 5.0f;
+        if (keyState[SDL_SCANCODE_DOWN]) square.rect.y += 5.0f;
+        if (keyState[SDL_SCANCODE_LEFT]) square.rect.x -= 5.0f;
+        if (keyState[SDL_SCANCODE_RIGHT]) square.rect.x += 5.0f;
 
-        // Gravidade
-        velocidade = velocidade + (gravidade * squarePeso);
+        // Velocidade de queda
+        square.velocidade = square.velocidade + (gravidade * square.peso);
+        triangle.velocidade = triangle.velocidade + (gravidade * triangle.peso);
 
         // Limitador de velocidade de queda
-        if (velocidade > 10.0f)
+        // Do quadrado
+        if (square.velocidade > 10.0f)
         { 
-            velocidade = 10.0f; 
+            square.velocidade = 10.0f; 
+        }
+
+        // Do triangulo
+        if (triangle.velocidade > 10.0f)
+        { 
+            triangle.velocidade = 10.0f; 
         }
 
         // Incremento da velocidade
-        square.y += velocidade;
+        square.rect.y += square.velocidade; // Do quadrado
+
+        square.rect.y += square.velocidade; // Do triangulo
 
         // Colisão com as PAREDES ESQUERDA E DIREITA
         for (const auto& parede : paredes)
         {
-            Colision::colisaoX(square, parede, oldX);   
+            Colision::colisaoX(square.rect, parede, oldX); // quadrado
+            Colision::colisaoX(triangle.rect, parede, oldX); // triangulo
         }
 
         // Colisão do TETO e CHÃO
         for (const auto& v : {chao, teto})
         {
-           Colision::colisaoY(square, v, oldY, velocidade, state);
+            Colision::colisaoY(square.rect, v, oldY, square.velocidade, square.state); // quadrado
+            Colision::colisaoY(triangle.rect, v, oldY, triangle.velocidade, triangle.state); // triangulo
 
-           if (keyState[SDL_SCANCODE_UP] && state) 
+           if (keyState[SDL_SCANCODE_UP] && square.state) 
            {
-                velocidade = -6.0f;
-                state = false;
+                square.velocidade = -6.0f;
+                square.state = false;
             }
         }
         
@@ -193,9 +225,9 @@ int main(int argc, char *argv[])
 
 /*
 
-if (square.x >= paredeDireita.x)
+if (square.rect.x >= paredeDireita.x)
 {
-    square.x = paredeDireita.x - square.x;   
+    square.rect.x = paredeDireita.x - square.x;   
 }
 
 */
