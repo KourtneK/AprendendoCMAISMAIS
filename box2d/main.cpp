1d3a7c6b328a82a7dab2db54ccb7440412184a90
KourtneK <lucasdaniel2009@gmail.com>
2026-04-24 21:38:06 -0400

    gravidade, inicialização de objetos, vetor de paredes, chão

--- box2d/main.cpp
+++ box2d/main.cpp
@@ -11,26 +11,77 @@
 
 Renderizador renderer;
 
+b2Vec2 gravidade{0.0f, 0.3f};
+
 struct Objetos
 {
-    b2BodyId body;
-    b2ShapeId shape;
-
     SDL_FRect rect;
     SDL_Color color;
 
-    float forca;
-    float peso;
-    float massa;
-    b2Vec2 velocidade{0.0f, 0.0f};
+    b2BodyId body;
+    b2ShapeId shape;
 
     bool state;
 };
 
 int main(int argc, char *argv[])
 {
-    
+    renderer.winOpen("box2d", 640, 480);
+
+    b2WorldId mundo;
+    b2WorldDef worldDef;
+    worldDef = b2DefaultWorldDef();
+
+    worldDef.gravity = gravidade;
+
+    mundo = b2CreateWorld(&worldDef);
+
+    renderer.loadTextures();
+
+    Objetos square;
+        square.rect = {220.0f, 240.0f, 50.0f, 50.0f};
+        b2BodyDef bodyDef = b2DefaultBodyDef();
+        b2ShapeDef shapeDef = b2DefaultShapeDef();
+        bodyDef.type = b2_dynamicBody;
+        b2Polygon box = b2MakeBox(25.0f, 25.0f);
+        bodyDef.position = {square.rect.x, square.rect.y};
+        square.body = b2CreateBody(mundo, &bodyDef);
+        square.shape = b2CreatePolygonShape(square.body, &shapeDef, &box);
+        
+    Objetos chao;
+        chao.rect = {0.0f, 450.0f, 640.0f, 50.0f}; // X, Y, W, H
+        b2BodyDef chaoDef = b2DefaultBodyDef();
+        chaoDef.type = b2_staticBody;
+        chaoDef.position = {320.0f, 475.0f};
+        chao.body = b2CreateBody(mundo, &chaoDef);
+        b2Polygon chaoBox = b2MakeBox(320.0f, 25.0f);
+        chao.shape = b2CreatePolygonShape(chao.body, &shapeDef, &chaoBox);
+
+    std::vector<SDL_FRect> paredes;
+        paredes.push_back(chao.rect);
+
+    while (renderer.running)
+    {
+        renderer.eventProcess();
+
+        b2World_Step(mundo, 1.0f / 60.0f, 4);
+
+        renderer.cleanScreen(0, 140, 0);
+
+        b2Vec2 position = b2Body_GetPosition(square.body);
+        square.rect.x = position.x - 25.0f;
+        square.rect.y = position.y - 25.0f;
+
+        renderer.drawWalls(paredes);
+        renderer.drawSquare(square.rect);
+
+        renderer.present();
+    }
+
+    b2DestroyWorld(mundo);
 
 
+
+    renderer.close();
     return 0;
 }
--- box2d/render.h
+++ box2d/render.h
@@ -18,40 +18,72 @@ struct Renderizador
     SDL_Texture* squareTexture = nullptr;
     SDL_Texture* wallTexture = nullptr;
 
-    // 1. Ferramenta: Inicializa o motor de vídeo e a janela
+    bool running = true;
+
+    // 1. Inicializa o motor de vídeo e a janela (Suporte a bordas puxáveis)
     void winOpen(const char* titulo, int w, int h)
     {
         SDL_Init(SDL_INIT_VIDEO);
 
+        // Título, Largura, Altura, Flags e ponteiros para a memória da janela/render
         SDL_CreateWindowAndRenderer(titulo, w, h, SDL_WINDOW_RESIZABLE, &window, &render);
     }
 
-    // 2. Ferramenta: Carrega o tilemap.bmp e preenche o seu vector
+    // Processa os eventos da janela e do teclado para saída
+    void eventProcess()
+    {
+        SDL_Event evento;
+        while (SDL_PollEvent(&evento))
+        {
+            if (evento.type == SDL_EVENT_QUIT)
+            {
+                running = false;
+            }
+
+            if (evento.type == SDL_EVENT_KEY_DOWN)
+            {
+                if (evento.key.key == SDLK_ESCAPE)
+                {
+                    running = false;
+                }
+            }
+        }
+    }
+
+    // 2. Converte BMP em dados de colisão e spawn
     void mapProcess(const char* caminho, std::vector<SDL_FRect>& paredes, float& playerX, float& playerY)
     {
         SDL_Surface* surfaceMap = SDL_LoadBMP(caminho);
 
         if (surfaceMap)
         {
+            // Converte para RGBA8888 para garantir que a leitura de cores seja padrão e não dê erro
             SDL_Surface* surfaceFormatada = SDL_ConvertSurface(surfaceMap, SDL_PIXELFORMAT_RGBA8888);
 
             SDL_DestroySurface(surfaceMap);
 
             if (surfaceFormatada)
             {
+                // Ponteiro para os dados crus dos pixels na memória linear
                 Uint32* pixels = (Uint32*)surfaceFormatada->pixels;
 
                 for (int y = 0; y < surfaceFormatada->h; y++)
                 {
                     for (int x = 0; x < surfaceFormatada->w; x++)
                     {
+                        // Multiplica Y pelo ponteiro do mapa processado e soma W + X
                         Uint32 pixelCor = pixels[y * surfaceFormatada->w + x];
+
                         Uint8 r, g, b, a;
 
                         SDL_GetRGBA(pixelCor, SDL_GetPixelFormatDetails(surfaceFormatada->format), NULL, &r, &g, &b, &a);
 
-                        if (r == 0 && g == 0 && b == 0) paredes.push_back({x * 50.0f, y * 50.0f, 50.0f, 50.0f});
-
+                        // Se o pixel for PRETO -> Guarda um retângulo de 50x50 no vetor
+                        if (r == 0 && g == 0 && b == 0)
+                        {
+                            paredes.push_back({x * 50.0f, y * 50.0f, 50.0f, 50.0f});
+                        }
+                        // Se o pixel for VERMELHO -> Define a coordenada de nascimento do quadrado
                         else if (r == 255 && g == 0 && b == 0)
                         {
                             playerX = x * 50.0f; playerY = y * 50.0f;
@@ -65,50 +97,65 @@ struct Renderizador
         }
     }
 
-    // 3. Ferramenta: Carrega as texturas dos BMPs
+    // 3. Traz as imagens da RAM (Surface) para a memória da GPU (Texture)
     void loadTextures()
     {
-        SDL_Surface* sSquare = SDL_LoadBMP("pigui.bmp");
-        SDL_Surface* sWall = SDL_LoadBMP("tijolo.bmp");
+        SDL_Surface* surfaceSquare = SDL_LoadBMP("pigui.bmp"); 
+        SDL_Surface* surfaceWall = SDL_LoadBMP("tijolo.bmp");
 
-        squareTexture = SDL_CreateTextureFromSurface(render, sSquare);
-        wallTexture = SDL_CreateTextureFromSurface(render, sWall);
+        // Log de erro no terminal caso o carregamento da textua falhe
+        if (!surfaceSquare || !surfaceWall)
+        {
+            SDL_Log("erro ao carregar bmp: %s", SDL_GetError());
+        }
 
-        SDL_DestroySurface(sSquare);
-        SDL_DestroySurface(sWall);
+        squareTexture = SDL_CreateTextureFromSurface(render, surfaceSquare);
+        wallTexture = SDL_CreateTextureFromSurface(render, surfaceWall);
+
+        // Limpa a RAM
+        SDL_DestroySurface(surfaceSquare);
+        SDL_DestroySurface(surfaceWall);
     }
 
-    // 4. Ferramentas de Desenho Granulares (Para você alternar com a física)
-    void limparTela(Uint8 r, Uint8 g, Uint8 b)
+    // 4. Escolhe a cor e limpa o frame anterior para o novo desenho
+    void cleanScreen(Uint8 r, Uint8 g, Uint8 b)
     {
-        SDL_SetRenderDrawColor(render, r, g, b, 255);
+        SDL_SetRenderDrawColor(render, r, g, b, 255); // Ex: Verde escuro (0, 140, 0)
 
         SDL_RenderClear(render);
     }
 
-    void desenharParedes(const std::vector<SDL_FRect>& paredes)
+    // 5. Desenha as paredes no vetor
+    void drawWalls(const std::vector<SDL_FRect>& paredes)
     {
-        for (const auto& parede : paredes) SDL_RenderTexture(render, wallTexture, NULL, &parede);
+        for (const auto& parede : paredes)
+        {
+            SDL_RenderTexture(render, wallTexture, NULL, &parede);
+        }
     }
 
+    // 6. Renderiza o Pinguim ou um retângulo azul se a imagem sumir
     void drawSquare(const SDL_FRect& rect)
     {
         if (squareTexture)
         {
-        SDL_RenderTexture(render, squareTexture, NULL, &rect);
+            SDL_RenderTexture(render, squareTexture, NULL, &rect);
         }
         else 
         {
-            SDL_SetRenderDrawColor(render, 0, 0, 140, 255); SDL_RenderFillRect(render, &rect);
+            SDL_SetRenderDrawColor(render, 0, 0, 140, 255); // Azul RGBA
+
+            SDL_RenderFillRect(render, &rect);
         }
     }
 
+    // 7. Renderiza tudo
     void present()
     {
         SDL_RenderPresent(render);
     }
 
-    // 5. Ferramenta: Cleanup final
+    // 8. Fecha o motor e limpa a memória ao sair
     void close()
     {
         SDL_DestroyTexture(squareTexture);
@@ -116,7 +163,7 @@ struct Renderizador
         SDL_DestroyRenderer(render);
         SDL_DestroyWindow(window);
 
-        SDL_Quit();
+        SDL_Quit(); // Para o motor de renderização
     }
 };
 
