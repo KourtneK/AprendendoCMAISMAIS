5a09aec2e8b57d8c428805d8f6cf7e2bf143aef6
KourtneK <lucasdanielrocha2009@gmail.com>
2026-05-14 11:40:02 -0400

    my own terminal emulator

---
+++ Terminal/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <SDL3\SDL.h>
+#include <SDL3\SDL_main.h>
+
+int main(int argc, const char** argv)
+{
+    std::cout << "ola mundo" << std::endl;
+
+    return 0;
+}
